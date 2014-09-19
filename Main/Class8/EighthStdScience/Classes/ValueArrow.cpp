711dba60cb1fccc8676f4f47aee3007cc8568ad0
pabitrapadhy <pabitrapadhycse@gmail.com>
2014-09-19 17:39:58 +0530

    string add crash

--- Main/Class8/EighthStdScience/Classes/ValueArrow.cpp
+++ Main/Class8/EighthStdScience/Classes/ValueArrow.cpp
@@ -30,31 +30,46 @@ bool ValueArrow::initWithFile(const std::string &fileName , const Rect &capInset
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	// force value label
+	_labelForce = LabelTTF::create("", "fonts/Maven Pro Black.otf", 30);
+	_labelForce->setHorizontalAlignment(TextHAlignment::LEFT);
+	_labelForce->setColor(Color3B::BLACK);
+	this->addChild(_labelForce, 1);
+
 	// storing the default content size to preserve it
 	_contWD = this->getContentSize().width;
 	_contHT = this->getContentSize().height;
 
-	// initialize arrows with zero value to hide them initially
-	if(_val == 0.0)
-		this->setVisible(false);
+	// calling adjust size to initialize the arrows with zero value, to hide them
+	adjustSize();
 
     return true;
 }
 
 void ValueArrow::adjustSize()
 {
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
 	if(_val == 0.0)
 	{
 		this->setVisible(false);
 		// resetting the content size to initial value stored in member variables
 		this->setContentSize(Size(_contWD , _contHT));
 	}
-
 	else
 	{
+		// sets invisible node to visible
 		this->setVisible(true);
 
 		auto val = fabs(_val);
+
+		// label for value of forces
+		// TODO: check why passing string fails and crashes the game
+		std::stringstream sstr;
+		sstr << (int)val << " N";
+//		_labelForce->setString(sstr.str());
+
 		// scaling the arrows
 		auto cont_wd = _contWD;
 		auto cont_ht = _contHT;
@@ -63,85 +78,20 @@ void ValueArrow::adjustSize()
 		// looks wierd otherwise.
 		this->setContentSize(Size(cont_wd + scalePercent , cont_ht));
 
-
-//
-//    std::stringstream sstr;
-//    sstr << (int)val << " N";
-//    _labelForce->setString(sstr.str());
-
-	// positioning the arrow left/right
-	float direction = _val < 0.0 ? -1 : 1;
-
+		// positioning the arrow left/right using rotation of node
+		float direction = _val < 0.0 ? -1 : 1;
 		if(direction == -1)
 		{
 			this->setRotation(-180);
-//			this->setPosition(Vec2(2*this->getPosition().x , this->getPosition().y));
+//			_labelForce->setAnchorPoint(Vec2::ANCHOR_BOTTOM_RIGHT);
+//			_labelForce->setPosition(Vec2(visibleSize.width/2 , visibleSize.height/2));
 		}
 		else
+		{
 			this->setRotation(0);
-
-//
-//    _shaft->setScaleX(scale);
-//    _shaft->setAnchorPoint(anchor);
-//    if (direction == -1)
-//        _shaft->setPosition(Vec2(-_shaft->getContentSize().width, 0.0));
-//    else
-//        _shaft->setPosition(Vec2(0.0, 0.0));
-//
-//    if (_head)
-//        removeChild(_head);
-//    auto head = DrawNode::create();
-//    _head = head;
-//    _head->setAnchorPoint(anchor);
-//    head->drawTriangle(Vec2(0.0, HEIGHT),
-//                        Vec2(0.0, -HEIGHT),
-//                        Vec2(direction * HEIGHT, 0.0),
-//                        Color4F(_color));
-//    head->setContentSize(Size(HEIGHT, 2*HEIGHT));
-//    addChild(_head);
-//
-//    if (direction == -1)
-//    {
-//        _head->setPosition(Vec2(-_shaft->getContentSize().width*scale + head->getContentSize().width*scaleHead, HEIGHT/2));
-//    }
-//    else
-//    {
-//        _head->setPosition(_shaft->getPosition() + Vec2(_shaft->getContentSize().width*scale, HEIGHT/2));
-//    }
-//    _head->setScaleX(scaleHead);
-//
-//
-//    if (direction == -1)
-//    {
-//        _labelName->setPosition(_head->getPosition() + Vec2( -2 * head->getContentSize().width * scaleHead, 0.0 ));
-//    }
-//    else
-//    {
-//        _labelName->setPosition(_head->getPosition() + Vec2(head->getContentSize().width * scaleHead, 0.0 ));
-//    }
-//    _labelName->setAnchorPoint(anchor);
-//
-//    if( _labelForce->getContentSize().width > _shaft->getContentSize().width * scale )
-//    {
-//        _labelForce->setPosition(_labelName->getPosition() + Vec2(direction * (_labelName->getContentSize().width + 10.0), 0.0 ) );
-//        _labelForce->setAnchorPoint(anchor);
-//    }
-//    else
-//    {
-//        _labelForce->setPosition(Vec2(direction * _shaft->getContentSize().width * scale/2 , 0.0));
-//        _labelForce->setAnchorPoint(Vec2::ANCHOR_MIDDLE_BOTTOM);
-//    }
-//
-//    setContentSize(_head->getContentSize());
-
+//			_labelForce->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
+//			_labelForce->setPosition(Vec2(visibleSize.width/2 , visibleSize.height/2));
+		}
 	}
 
-}
-
-void ValueArrow::defaultArrowSize()
-{
-	auto cont_wd = this->getContentSize().width;
-	auto cont_ht = this->getContentSize().height;
-
-	this->setContentSize(Size(cont_wd , cont_ht));
 }
--- Main/Class8/EighthStdScience/Classes/ValueArrow.h
+++ Main/Class8/EighthStdScience/Classes/ValueArrow.h
@@ -7,11 +7,10 @@ class ValueArrow : public cocos2d::extension::Scale9Sprite
 {
     float _val;
     cocos2d::LabelTTF * _labelForce;
+    float _contWD;
+	float _contHT;
 
 public :
-    float _contWD;
-    float _contHT;
-
     static ValueArrow * create(const std::string &fileName , const cocos2d::Rect &capInset);
     ValueArrow() :
     	_val(0.0),
@@ -21,7 +20,7 @@ public :
 
     bool initWithFile(const std::string &fileName , const cocos2d::Rect &capInset);
     void adjustSize();
-    void defaultArrowSize();
+
     // TODO: understand its implementation and logic
     void setValue(float f)
     {
