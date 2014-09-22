b9980fa0adc7a32da608f9c08fe3581912586845
sreeram <kandimalla.sreeram@gmail.com>
2014-09-22 12:31:44 +0530

    fix person falling code

--- Main/Class8/EighthStdScience/Classes/SpriteLayer.cpp
+++ Main/Class8/EighthStdScience/Classes/SpriteLayer.cpp
@@ -274,7 +274,7 @@ void SpriteLayer::changeForceValue(float value)
     if(value != _forceExternalValue)
     {
         _forceExternalValue = value;
-        if (_forceExternalValue > 0.0)
+        if (fabs(_forceExternalValue) > 0.0)
            _personFell = false; 
         addPersonOfForce();
     }
@@ -314,7 +314,7 @@ void SpriteLayer::update(float dt)
     if (_periodicCB && !_periodicCB())
         return;
 
-    if (fabs(_velocity) > MAX_SPEED)
+    if (!_personFell && fabs(_velocity) > MAX_SPEED)
     {
         _personFell = true;
         
