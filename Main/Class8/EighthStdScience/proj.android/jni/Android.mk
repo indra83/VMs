56172c024762491182394303646badecb3b80930
sreeram <kandimalla.sreeram@gmail.com>
2014-10-01 19:43:05 +0530

    initial import

--- Main/Class8/EighthStdScience/proj.android/jni/Android.mk
+++ Main/Class8/EighthStdScience/proj.android/jni/Android.mk
@@ -22,6 +22,7 @@ LOCAL_SRC_FILES := eighthstdscience/main.cpp \
                    ../../Classes/Util.cpp \
                    ../../Classes/PopUpScene.cpp \
                    ../../Classes/ValueArrow.cpp \
+                   ../../Classes/AdvancedSprite.cpp \
                    ../../Classes/SpriteLayer.cpp
                
 
